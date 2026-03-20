14d1b080221087812c77ac81961902d6a2e7fe27
TimYaoOo <timlaoyao@163.com>
2026-03-20 16:28:16 +0800

    chore: add src/lib/types/schema.d.ts

---
+++ src/lib/types/schema.d.ts
@@ -0,0 +1,38 @@
+import type { GridRect } from "./designer";
+
+export interface BeeNodeGridSchema {
+  rows: number;
+  cols: number;
+  gap: number;
+}
+
+export interface BeeNodeSchema {
+  id: string;
+  name: string;
+  rect: GridRect;
+  zIndex: number;
+  grid?: BeeNodeGridSchema;
+  component: Record<string, unknown>;
+  children: BeeNodeSchema[];
+}
+
+export interface BeeLayerGroupSchema {
+  id: string;
+  name: string;
+  parentId: string | null;
+  nodeIds: string[];
+  groupIds: string[];
+}
+
+export interface BeeDataViewSchema {
+  title: string;
+  modifyTime: string;
+  width: number;
+  height: number;
+  rows: number;
+  cols: number;
+  gap: number;
+  scalePercent: number;
+  root: BeeNodeSchema;
+  layerGroups: BeeLayerGroupSchema[];
+}
